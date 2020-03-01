6273e268f75426a661c96dac4ec1dc74322543e8
Eric Xu <xeric.2002@gmail.com>
2020-03-01 18:33:10 -0500

    Fixed weird Toolbar error

--- app/src/main/java/com/example/simplenotes/MainActivity.java
+++ app/src/main/java/com/example/simplenotes/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.simplenotes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rV_notesList;
+    private String[] testStrings = {"Title1", "Title2", "Title3", "Title4", "Title5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        rV_notesList = findViewById(R.id.rV_notesList);
+        MyAdapter myAdapter = new MyAdapter(this, testStrings);
+        rV_notesList.setAdapter(myAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
     }
 }
