dd6e6350deb081d7facaab17fa7fbb69ee6cbc75
Eric Xu <xeric.2002@gmail.com>
2020-03-02 13:01:32 -0500

    Re-added Note.java

---
+++ app/src/main/java/com/example/simplenotes/Note.java
@@ -0,0 +1,33 @@
+package com.example.simplenotes;
+
+public class Note {
+    private String name;
+    private String fileName;
+
+    public Note(String name) {
+        this(name, name);
+    }
+
+    public Note(String name, String fileName) {
+        this.name = name;
+        this.fileName = fileName;
+    }
+
+    //** Methods ****************************************************
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
